a4988dbeecbebb94108288ba7f648d9baffe0df4
veselinfilev <veselin.filev1@abv.bg>
2024-03-16 14:54:57 +0200

    Add courses module

---
+++ src/app/courses/courses.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CoursesRoutingModule } from './courses-routing.module';
+import { CatalogComponent } from './catalog/catalog.component';
+import { DetailsComponent } from './details/details.component';
+import { EditComponent } from './edit/edit.component';
+import { CreateComponent } from './create/create.component';
+
+
+@NgModule({
+  declarations: [
+    CatalogComponent,
+    DetailsComponent,
+    EditComponent,
+    CreateComponent
+  ],
+  imports: [
+    CommonModule,
+    CoursesRoutingModule
+  ]
+})
+export class CoursesModule { }
