5af9f2a933c1e1423cb48b869c55ee37ad262c3e
veselinfilev <veselin.filev1@abv.bg>
2024-03-18 19:53:32 +0200

    Added search and sort params

--- src/app/courses/catalog/catalog.component.ts
+++ src/app/courses/catalog/catalog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CoursesService } from '../courses.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-catalog',
@@ -7,13 +8,76 @@ import { CoursesService } from '../courses.service';
   styleUrls: ['./catalog.component.css'],
 })
 export class CatalogComponent implements OnInit {
+  searchForm: FormGroup;
+  searchQueryValue:string='';
   courses: any | null = null;
+  offset: number = 0;
+  pageSize: number = 6;
+  currentPage = 0;
+  isSelectedLatest: boolean = false;
+  isSelectedHightToLow: boolean = false;
+  isSelectedLowToHeight: boolean = false;
 
-  constructor(private coursesServices: CoursesService) {}
+  constructor(private coursesServices: CoursesService, private fb: FormBuilder) {
+    this.searchForm = this.fb.group({
+      searchQuery: ['']
+    })
+  }
 
   ngOnInit(): void {
-    this.coursesServices.getCourses().subscribe((data) => {
-      this.courses = data;
-    });
+    this.pageInfo();
+  }
+
+  pageInfo() {
+    return this.coursesServices
+      .getCurrentPageCourses(
+        this.offset,
+        this.pageSize,
+        this.isSelectedLatest,
+        this.isSelectedHightToLow,
+        this.isSelectedLowToHeight,
+        this.searchQueryValue
+      )
+      .subscribe((data) => {
+        this.courses = data;
+      });
+  }
+
+  search() {
+    this.searchQueryValue = this.searchForm.get('searchQuery')?.value;
+    this.pageInfo();
+  }
+
+  clickPreviousBtn() {
+    this.currentPage -= 1;
+    this.offset = this.currentPage * this.pageSize;
+    this.pageInfo();
+  }
+
+  clickNextBtn() {
+    this.currentPage += 1;
+    this.offset = this.currentPage * this.pageSize;
+    this.pageInfo();
+  }
+
+  clickLatest() {
+    this.isSelectedHightToLow = false;
+    this.isSelectedLowToHeight = false;
+    this.isSelectedLatest = !this.isSelectedLatest;
+    this.pageInfo();
+  }
+
+  clickHightToLow() {
+    this.isSelectedLowToHeight = false;
+    this.isSelectedLatest = false;
+    this.isSelectedHightToLow = !this.isSelectedHightToLow;
+    this.pageInfo();
+  }
+
+  clickLowToHight() {
+    this.isSelectedHightToLow = false;
+    this.isSelectedLatest = false;
+    this.isSelectedLowToHeight = !this.isSelectedLowToHeight;
+    this.pageInfo();
   }
 }
--- src/app/courses/courses.service.ts
+++ src/app/courses/courses.service.ts
@@ -6,6 +6,9 @@ import { HttpClient } from '@angular/common/http';
 })
 export class CoursesService {
   URL = 'http://localhost:3030/data/courses';
+  pageInfo: string = '';
+  sortParams: string = '?';
+  searchParams:string='';
 
   constructor(private http: HttpClient) {}
 
@@ -15,5 +18,31 @@ export class CoursesService {
 
   getOneCourse(courseId: string | null) {
     return this.http.get(`${this.URL}/${courseId}`);
+  }
+
+  getCurrentPageCourses(
+    offset: number,
+    pageSize: number,
+    latest: boolean,
+    hightToLow: boolean,
+    lowToHeight: boolean,
+    searchValue:string
+  ) {
+
+    if (latest) {
+      this.sortParams = '?sortBy=_createdOn desc&';
+    } else if (hightToLow) {
+      this.sortParams = '?sortBy=price desc&';
+    } else if (lowToHeight) {
+      this.sortParams = '?sortBy=price&';
+    } else {
+      this.sortParams = '?';
+    }
+
+    this.searchParams = `&where=title LIKE "${searchValue}" OR lecture LIKE "${searchValue}"`
+
+    this.pageInfo = `offset=${offset}&pageSize=${pageSize}`;
+
+    return this.http.get(`${this.URL}${this.sortParams}${this.pageInfo}${this.searchParams}`);
   }
 }
