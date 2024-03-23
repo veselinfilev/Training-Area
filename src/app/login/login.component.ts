3b72d622f4a17a04eab6ca8c5e6214bfd6d25bb3
veselinfilev <veselin.filev1@abv.bg>
2024-03-23 11:38:22 +0200

    Add login functionality

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,25 +1,34 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import {  Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-loginForm:FormGroup;
+  loginForm: FormGroup;
 
-constructor(private fb:FormBuilder){
-  this.loginForm=this.fb.group({
-    username:['',[Validators.required]],
-    password:['',Validators.required]
-  })
-}
-
-onSubmit(){
-  if(this.loginForm.valid){
-    console.log(this.loginForm.value);
-    
+  constructor(private fb: FormBuilder, private authService: AuthService,private router:Router) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required,Validators.email]],
+      password: ['', Validators.required],
+    });
   }
-}
+
+  onSubmit() {
+    const email = this.loginForm.get('email')?.value;
+    const password = this.loginForm.get('password')?.value;
+    this.authService.login(email, password).subscribe(v=>{
+      const { accessToken, email, username, _id  } = v;
+      
+      localStorage.setItem('user',JSON.stringify({accessToken, email, username, _id}));
+      this.router.navigate(['/'])
+      
+    },error =>{
+      alert(error.error.message);
+    })
+  }
 }
