808cdd11b24527546a3421bf1b2a456d25a91706
veselinfilev <veselin.filev1@abv.bg>
2024-03-23 11:40:09 +0200

    Add dynamic rendering based on being a user or guest

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  styleUrls: ['./navigation.component.css'],
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
+  isUser: boolean = false;
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.isLoggedIn().subscribe((isLoggedIn) => {
+      this.isUser = isLoggedIn;
+    });
+  }
+
+  handleLogoutClick():void{
+    this.authService.logout();
+  }
 
 }
