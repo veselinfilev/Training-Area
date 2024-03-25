6a004341c4a0296993f87e40e36fed34c94b7324
veselinfilev <veselin.filev1@abv.bg>
2024-03-25 22:49:14 +0200

    Change profile page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent {
+  isClickedFirst: boolean = false;
+  isClickedSecond: boolean = false;
 
+  onClick(ev: Event) {
+    const target = ev.target as HTMLElement;
+
+    if (target.className.includes('first')) {
+      this.isClickedFirst = !this.isClickedFirst;
+      this.isClickedSecond = false;
+    } else {
+      this.isClickedSecond = !this.isClickedSecond;
+      this.isClickedFirst = false;
+    }
+  }
 }
